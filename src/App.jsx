ece5e43b94c551a6e6f5f4ad7a9bfd12f0406374
InomjonDev <inomjonismanaliev@gmail.com>
2023-11-21 21:13:46 +0500

    Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { Route, Routes } from "react-router-dom";
-import "./App.css";
-import { Login, Main, Register } from "./components";
+import { Login, Main, Register, Navbar } from "./components";
 
 function App() {
 	return (
 		<div>
+			<Navbar />
 			<Routes>
 				<Route path="/" element={<Main />} />
 				<Route path="/login" element={<Login />} />
